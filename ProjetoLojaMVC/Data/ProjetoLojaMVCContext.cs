2a6fc0db5a85e02166ac3a11d90223d9e1aea9a6
HenriqueGreghi <henriquevsg@gmail.com>
2021-11-24 23:08:25 -0300

    Scaffolding

---
+++ ProjetoLojaMVC/Data/ProjetoLojaMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoLojaMVC.Models;
+
+namespace ProjetoLojaMVC.Data
+{
+    public class ProjetoLojaMVCContext : DbContext
+    {
+        public ProjetoLojaMVCContext (DbContextOptions<ProjetoLojaMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoLojaMVC.Models.Departament> Departament { get; set; }
+    }
+}
--- ProjetoLojaMVC/Startup.cs
+++ ProjetoLojaMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoLojaMVC.Data;
 
 namespace ProjetoLojaMVC
 {
@@ -33,6 +35,9 @@ namespace ProjetoLojaMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoLojaMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoLojaMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
